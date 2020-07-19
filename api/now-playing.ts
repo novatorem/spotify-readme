5a6d428f53fb98fd7f5176fcb7e15793288cbbb0
novatorem <anovaec@gmail.com>
2020-07-19 18:09:57 -0400

    Fix iteming

--- api/now-playing.ts
+++ api/now-playing.ts
@@ -1,18 +1,19 @@
-import { NowRequest, NowResponse } from "@vercel/node";
 import { renderToString } from "react-dom/server";
+import { NowRequest, NowResponse } from "@vercel/node";
 import { decode } from "querystring";
-import { Player } from "../components/NowPlaying";
 import { nowPlaying } from "../utils/spotify";
+import { Player } from "../components/NowPlaying";
+
 
 export default async function (req: NowRequest, res: NowResponse) {
-  const {
-    item = {},
-    is_playing: isPlaying = false,
+  const { 
+    item = {}, 
+    is_playing: isPlaying = false, 
     progress_ms: progress = 0,
-  } = await nowPlaying();
+ } = await nowPlaying();
 
   const params = decode(req.url.split("?")[1]) as any;
-
+  
   if (params && typeof params.open !== "undefined") {
     if (item && item.external_urls) {
       res.writeHead(302, {
@@ -24,16 +25,22 @@ export default async function (req: NowRequest, res: NowResponse) {
   }
 
   res.setHeader("Content-Type", "image/svg+xml");
-  res.setHeader("Cache-Control", "s-maxage=1, stale-while-revalidate");
+  // res.setHeader("Cache-Control", "s-maxage=1, stale-while-revalidate");
+  res.setHeader("Cache-Control", "no-cache");
 
-  const { duration_ms: duration, name: track } = item;
+  const {
+    duration_ms: duration,
+    name: track,
+  } = item;
   const { images = [] } = item.album || {};
 
   const cover = images[images.length - 1]?.url;
   let coverImg = null;
   if (cover) {
-    const buff = await (await fetch(cover)).arrayBuffer();
-    coverImg = `data:image/jpeg;base64,${Buffer.from(buff).toString("base64")}`;
+    const buff = await(await fetch(cover)).arrayBuffer();
+    coverImg = `data:image/jpeg;base64,${Buffer.from(buff).toString(
+      "base64"
+    )}`;
   }
 
   const artist = (item.artists || []).map(({ name }) => name).join(", ");
